8d89f45ac9460b9035d87f38b676368a7df2443f
FabianAdrian <38763737+FabianAdrian@users.noreply.github.com>
2018-07-02 13:42:53 +0300

    Lesson 10

--- Practice LESSON  10 .sql
+++ Practice LESSON  10 .sql
@@ -12,5 +12,28 @@ SELECT exam_id
 ,Round(AVG(marks))"Average"
 FROM ad_exam_results
 group by exam_id
-order by exam_id
+order by exam_id;
+                                  --6--
+SELECT course_id,COUNT(*)
+FROM ad_student_course_details
+group by course_id;
+                                  --7--
+SELECT exam_id, course_id,AVG(marks)
+FROM ad_exam_results
+group by exam_id,course_id
+HAVING AVG(marks)>85;
+                                  --8--
+SELECT MAX(salary)-MIN(salary) Difference
+FROM ad_faculty_details;
+                                  --9--
+Select MAX(AVG(marks))
+FROM ad_exam_results
+group by marks;
+                                  --10--
+SELECT course_id,MIN(marks)
+FROM ad_exam_results
+WHERE  course_id IN (190,191,192) 
+group by course_id
+HAVING MIN(marks)>75
+
 
